ed342c0f3d802c42ba1bc5aba847c6220f525052
Matt Miller <matt.f.miller@live.com>
2018-06-01 08:35:58 -0700

    Add and Pass test for correctly instantiating team object.

--- src/main/java/models/Team.java
+++ src/main/java/models/Team.java
@@ -1,4 +1,17 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Team {
+    private ArrayList<String> members = new ArrayList<>();
+    private String name;
+    private String description;
+    private int id;
+    private ArrayList<Team> teamsList = new ArrayList<>();
+
+    public Team() {
+        teamsList.add(this);
+        this.id = teamsList.size();
+    }
+
 }
--- src/test/java/models/TeamTest.java
+++ src/test/java/models/TeamTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,11 @@ public class TeamTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void newTeamObjectIsInstantiatedCorrectly_true() throws Exception {
+        Team team = new Team();
+        assertTrue(team instanceof Team);
     }
 }
