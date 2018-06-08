a987d3ef2916c0ac47c04160f09ea44d461dc99f
Matt Miller <matt.f.miller@live.com>
2018-06-08 09:28:41 -0700

    Add method signatures to team and method daos.

--- src/main/java/dao/MemberDao.java
+++ src/main/java/dao/MemberDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.Member;
+
+import java.util.List;
+
 public interface MemberDao {
+    // LIST
+    List<Member> getAllMembersByTeamId(int teamId);
+
+    // CREATE
+    void add(Member member);
+
+    // READ
+    Member findById(int id);
+
+    // UPDATE
+    void update(int id, String name, String skill, int teamId);
+
+    // DELETE
+    void deleteById(int id);
+    void deleteByTeamId(int teamId);
 }
--- src/main/java/dao/TeamDao.java
+++ src/main/java/dao/TeamDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.Member;
+import models.Team;
+
+import java.util.List;
+
 public interface TeamDao {
+    // LIST
+    List<Team> getAll();
+
+    // CREATE
+    void add(Team team);
+
+    // READ
+    Team findById(int id);
+
+    // UPDATE
+    void update(int id, String name, String description);
+
+    // DELETE
+    void deleteById(int id);
 }
