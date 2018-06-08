091b80e52a699a8ca3e77e70b6eec96c1a26c726
Matt Miller <matt.f.miller@live.com>
2018-06-08 10:41:38 -0700

    Add and pass test on Sql2oMemberDao that adding a member to database sets id.

--- src/main/java/dao/Sql2oMemberDao.java
+++ src/main/java/dao/Sql2oMemberDao.java
@@ -1,5 +1,56 @@
 package dao;
 
-public class Sql2oMemberDao {
+import models.Member;
+import org.sql2o.*;
+
+import java.util.List;
+
+public class Sql2oMemberDao implements MemberDao {
+
+    private final Sql2o sql2o;
+
+    public Sql2oMemberDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public List<Member> getAllMembersByTeamId(int teamId) {
+        return null;
+    }
+
+    @Override
+    public void add(Member member) {
+        String sql = "INSERT INTO members (name, skill, teamId) VALUES (:name, :skill, :teamId)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(member)
+                    .executeUpdate()
+                    .getKey();
+            member.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+    @Override
+    public Member findById(int id) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String name, String skill, int teamId) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void deleteByTeamId(int teamId) {
+
+    }
 
 }
---
+++ src/test/java/dao/Sql2oMemberDaoTest.java
@@ -0,0 +1,60 @@
+package dao;
+
+import models.Member;
+import models.Team;
+import org.junit.*;
+import org.sql2o.*;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class Sql2oMemberDaoTest {
+    private Sql2oMemberDao memberDao;
+    private Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        memberDao = new Sql2oMemberDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    public Member setupNewMember(){
+        return new Member("Bill", "Bass", 1);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
+    }
+
+    @Test
+    public void addingMemberSetsId() {
+        Member testMember = setupNewMember();
+        int originalMemberId = testMember.getId();
+        memberDao.add(testMember);
+        assertNotEquals(originalMemberId, testMember.getId());
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void getAllMembersByTeamId() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+
+    @Test
+    public void deleteByTeamId() {
+    }
+}
